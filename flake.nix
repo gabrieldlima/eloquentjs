6af037515b209e7e5e0308b163122cb3fbc4715b
Gabriel Lima <gabrielgsdlima@gmail.com>
2024-05-01 19:33:33 -0300

    Flake that defines a development environment with Node.js

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_22
+        onefetch
+      ];
+
+      shellHook = ''
+        onefetch
+        exec fish
+      '';
+    };
+  };
+}
